61daf161885085de7ae18081746c2293142259e3
Antilef <fi.antilef@gmail.com>
2024-12-16 23:46:50 -0300

    Add landing page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
-    {path: '',redirectTo: 'login',pathMatch:'full'},
+    {path: '',component: LandingComponent},
     {path: 'login', component: LoginComponent}
 ];
