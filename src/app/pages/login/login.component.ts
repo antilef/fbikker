05483cf194b6cb1c1f39f7a804957aa96ac4b5e3
Antilef <fi.antilef@gmail.com>
2024-12-20 23:25:36 -0300

    Add validation to email and password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+
 })
 export class LoginComponent {
+    emailSend:string = '';
+    passwordSend:string = '';
+    logInForm: FormGroup;
+
+    constructor(){
+        this.logInForm = new FormGroup({
+            email: new FormControl(this.emailSend,[
+                Validators.required,
+                Validators.email
+            ]),
+            password: new FormControl(this.passwordSend,[
+                Validators.minLength(8)
+            ])
+        })
+
+    }
+
+    get email(){
+        return this.logInForm.get('email');
+    }
+
+    get password(){
+        return this.logInForm.get('password')
+    }
+    
+    logIn(){
+
+    }
 
 }
