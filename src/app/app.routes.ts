b0a6943fe89cf195870c8720d47dabd1727f9b64
Antilef <fi.antilef@gmail.com>
2025-01-08 23:19:48 -0300

    feat: add rents route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthGuard } from './services/auth-guard.guard';
+import { RentsComponent } from './pages/rents/rents.component';
 
 export const routes: Routes = [
     {path: '',component: LandingComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register',component: RegisterComponent},
-    {path: 'home',component: HomeComponent,canActivate:[AuthGuard]}
+    {path: 'home',component: HomeComponent,canActivate:[AuthGuard]},
+    {path: 'rents',component: RentsComponent,canActivate:[AuthGuard]}
 ];
