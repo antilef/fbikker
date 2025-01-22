22e1b684b2c5ed18fd65dc6204c0d26c27cbec6c
Antilef <fi.antilef@gmail.com>
2025-01-22 20:58:21 -0300

    feat: add modal to create rent

--- src/app/components/add-rent-button/add-rent-button.component.ts
+++ src/app/components/add-rent-button/add-rent-button.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit,TemplateRef } from '@angular/core';
 import { Bike } from '../../model/entity.interface';
 import { BikeService } from '../../services/api/bike.service';
+import { NgbDatepickerModule,NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-rent-button',
-  imports: [],
+  imports: [NgbDatepickerModule],
   templateUrl: './add-rent-button.component.html',
   styleUrl: './add-rent-button.component.css'
 })
 export class AddRentButtonComponent implements OnInit {
   @Input('name') name: string = ''; 
   bikes: Bike[] = [];
-
+  private modalService = inject(NgbModal);
 
   constructor(private bikeService: BikeService){
   
@@ -23,5 +24,16 @@ export class AddRentButtonComponent implements OnInit {
       console.log("bikes: ..",this.bikes)
     })
   }
+
+  open(content: TemplateRef<any>) {
+		this.modalService.open(content, { ariaLabelledBy: 'Add rent',centered: true }).result.then(
+			(result) => {
+				console.log(result)
+			},
+			(reason) => {
+				console.log("reason",reason)
+			},
+		);
+	}
 
 }
